9dd0f95d6e51a2b6df65744150aa33eb404b279f
dgreenbe77 <dgreenbe777@gmail.com>
2014-02-23 23:42:27 -0500

    Update cashier.rb

--- cashier.rb
+++ cashier.rb
@@ -4,15 +4,15 @@ amount_due = gets.chomp.to_f
 puts "What is the amount tendered?"
 tendered = gets.chomp.to_f
 
-total =  sprintf "%.2f" , tendered - amount_due
+total =  tendered - amount_due
+new_total = sprintf('%.2f', -total)
+other_total = sprintf('%.2f', total)
 @time = Time.new.strftime("%D %H:%M")
 
-if amount_due > tendered
-  puts "WARNING CUSTOMER OWES YOU $#{total} MORE SCRILLA!"
+if total < 0
+  puts "WARNING CUSTOMER OWES YOU $#{new_total} MORE SCRILLA!"
 else
 puts "Thank You!"
-puts "The total change due is $#{total}"
+puts "The total change due is $#{other_total}"
 puts @time
 end
-
-
